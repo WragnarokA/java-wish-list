dbc29a99602eb466adad33fba4f345ed3e422ae2
wilmer <wilmer.arango.a@gmail.com>
2023-10-26 15:33:21 +0200

    GiftList

---
+++ src/ChristmasGifts/GiftList.java
@@ -0,0 +1,14 @@
+package ChristmasGifts;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GiftList {
+    public static void main(String[] args) {
+        ArrayList<GiftList> wishList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        // Carica la lista dei desideri se esiste
+    }
+}
